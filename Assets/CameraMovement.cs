dedced5749ca0bb2c62efdd553538cda35e0101d
lisyariv <lisyariv19@gmail.com>
2025-01-18 12:19:53 -0500

    I tried to code 3rd person perspective
    I failed at doing it :(

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float mouseSensitivity;
+    public float xRot = 0f;
+    public float minY = -90f;
+    public float maxY = 90f;
+    public float distanceFromCamera = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + new Vector3(0, distanceFromCamera, 0);
+        MouseLook();
+    }
+
+    void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, minY, maxY);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
