a1c9496c90c6a466d9af219fff41d46da7fd3b35
lisyariv <lisyariv19@gmail.com>
2025-01-25 13:05:54 -0500

    Made the frog fly

--- Assets/Script/FrogMovement.cs
+++ Assets/Script/FrogMovement.cs
@@ -7,29 +7,48 @@ public class FrogMovement : MonoBehaviour
     public Vector3 moveDirection;
     public float speed = 5.0f;
     public Vector3 jumpMovement;
+    public Vector3 flyMovement;
     public bool canJump;
+    public bool canFly;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         canJump = false;
+        canFly = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+       /* if (player.transform.position.y > 10)
+        {
+            canFly = false;
+        }
+        else
+        {
+            canFly = true;
+        }*/
+       //Moving left to right
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector3(x, 0, z);
         transform.Translate(moveDirection * Time.deltaTime * speed);
-        if (canJump)
+        //Jumping 
+
+        if (canJump == true && Input.GetKey(KeyCode.Space))
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                canJump = false;
-                GetComponent<Rigidbody>().AddForce(jumpMovement);
-            }
-        }   
+            canJump = false;
+            GetComponent<Rigidbody>().AddForce(jumpMovement);
+        }
+
+        //Flying
+
+        if (canFly == true && Input.GetKey(KeyCode.F))
+        {
+            GetComponent<Rigidbody>().AddForce(flyMovement);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,6 +56,7 @@ public class FrogMovement : MonoBehaviour
         if(collision.gameObject.tag == "Ground")
         {
             canJump = true;
+            canFly = true;
         }
     }
     
