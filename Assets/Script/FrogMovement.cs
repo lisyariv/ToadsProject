364ab0b791c88821037bc98673d5fb0cbf126583
lisyariv <lisyariv19@gmail.com>
2025-01-25 11:32:40 -0500

    Made the frog jump!

--- Assets/Script/FrogMovement.cs
+++ Assets/Script/FrogMovement.cs
@@ -6,10 +6,12 @@ public class FrogMovement : MonoBehaviour
 {
     public Vector3 moveDirection;
     public float speed = 5.0f;
+    public Vector3 jumpMovement;
+    public bool canJump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canJump = false;
     }
 
     // Update is called once per frame
@@ -20,5 +22,22 @@ public class FrogMovement : MonoBehaviour
 
         moveDirection = new Vector3(x, 0, z);
         transform.Translate(moveDirection * Time.deltaTime * speed);
+        if (canJump)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                canJump = false;
+                GetComponent<Rigidbody>().AddForce(jumpMovement);
+            }
+        }   
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+    
 }
