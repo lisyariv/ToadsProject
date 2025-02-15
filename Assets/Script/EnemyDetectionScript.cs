1b37c551df21fe60eacdeb5757eb75f154d7c534
thor1000p <Dikesam9@gmail.com>
2025-02-15 14:59:53 -0500

    commiting for now
    This reverts commit c128ae298db184e71d880829e9ead36e51a93d0b.

--- Assets/Script/EnemyDetectionScript.cs
+++ Assets/Script/EnemyDetectionScript.cs
@@ -10,13 +10,12 @@ public class EnemyDetectionScript : MonoBehaviour
     public float attackTimer;
     public IEnumerator attack;
     public GM gameManager;
-    public FrogMovement frog;
+    public GameObject 
     // Start is called before the first frame update
     void Start()
     {
         status = GameObject.Find("Status").GetComponent<TMP_Text>();
         status.text = "";
-        frog = GameObject.Find("Player").GetComponent<FrogMovement>();
     }
 
     // Update is called once per frame
@@ -29,11 +28,7 @@ public class EnemyDetectionScript : MonoBehaviour
         if (other.gameObject.tag == "Player" && gameManager.isInBush == false )
         {
             status.text = "You have been Spotted";
-            attackTimer += Time.deltaTime;
-            if (attackTimer >= 4f)
-            {
-                
-            }
+            //attack
 
         }
     }
