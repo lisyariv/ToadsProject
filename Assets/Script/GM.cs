8ff00bec641ab663353e1b29271a3c4b2551c23a
lisyariv <166237787+lisyariv@users.noreply.github.com>
2025-06-07 22:02:22 -0400

    Revert "Merge branch 'Sam' into LisyariBranch"
    This reverts commit 07ddadb80a3a023535b5967a70f37130e8abb889, reversing
    changes made to 18233607e9b179230449598d2d5901181393bc90.

--- Assets/Script/GM.cs
+++ Assets/Script/GM.cs
@@ -24,12 +24,7 @@ public class GM : MonoBehaviour
     public TMP_Text KeysText;
 
     public FrogMovement frog;
-    public AudioSource backgroundAudio;
-    public AudioClip backgroundSound;
-
-    
-
-
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -44,9 +39,6 @@ public class GM : MonoBehaviour
         isNightTime = false;
         GameText.text = "Your energy is low! Find and consume prey to boost your energy.";
         KeysText.text = "Controls: WASD or Arrow Keys to move, SPACE to jump.";
-        backgroundAudio.clip = backgroundSound;
-        backgroundAudio.Play();
-
 
     }
 
