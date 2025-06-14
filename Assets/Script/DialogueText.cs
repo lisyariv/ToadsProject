2b53979c2f8fbcee182e20da8659188fb3576e6b
lisyariv <166237787+lisyariv@users.noreply.github.com>
2025-06-14 01:59:50 -0400

    Working on the Backstory scene (wip)

--- Assets/Script/DialogueText.cs
+++ Assets/Script/DialogueText.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DialogueText : MonoBehaviour
 {
     public List<string> DialogueTxt;
+    public List<Sprite> sceneImages;
+    public Image currentImage;
     public TMP_Text Dialogue;
     public int index;
     // Start is called before the first frame update
@@ -27,6 +30,7 @@ public class DialogueText : MonoBehaviour
         if(index < DialogueTxt.Count)
         {
             Dialogue.text = DialogueTxt[index];
+            currentImage.sprite = sceneImages[index];
             index++;
         }
         else
